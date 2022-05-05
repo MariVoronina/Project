76fc42fd38b274f806cf9ab949a94590c6f53826
Арина Вычегжанина <99398012+ArinaVychegzhanina@users.noreply.github.com>
2022-05-05 12:18:58 +0300

    Add files via upload

--- Project.py
+++ Project.py
@@ -3,18 +3,65 @@ from tkinter import messagebox
 from tkinter.ttk import Combobox
 from tkinter.ttk import Checkbutton
 from tkinter.ttk import Radiobutton
+from tkinter import Menu
 def clicked1():
-    messagebox.showinfo('Заголовок', 'reggadf')
+    messagebox.showinfo('Заголовок', 'Здрасте')
 
 
 def clicked2():
     lbl.configure(text="Я же просил...")
 
 
+gui = Tk(className='Python Examples - Window Color')
+# set window size
+gui.geometry("400x200")
+#set window color
+gui.configure(bg='blue')
+gui.mainloop()
+
+
+types = ["Banks", "Biotechnology", "Cars", "Hotels", "IT", "Entertainments", "Clothes", "Food"]
+countries = ["United States", "Russia", "China", "United Kingdom", "France", "Japan"]
+
+window = Tk()
+window.title("Акации по акции")
+window.geometry('600x450')
+menu = Menu(window)
+new_item = Menu(menu)
+strana = Menu(new_item)
+new_item.add_command(label='График', command=clicked1)
+new_item.add_separator()
+for i in countries:
+    strana.add_command(label=i)
+    strana.add_separator()
+new_item.add_cascade(label='по странам', menu=strana)
+new_item.add_separator()
+kategoria = Menu(new_item)
+for i in types:
+    kategoria.add_command(label=i)
+    kategoria.add_separator()
+new_item.add_cascade(label='по категориям', menu=kategoria)
+menu.add_cascade(label='Сортировать', menu=new_item)
+window.config(menu=menu)
+from PIL import ImageTk, Image
+img = Image.open('конор.jpg')
+width = 600
+ratio = (width / float(img.size[0]))
+height = int((float(img.size[1]) * float(ratio)))
+imag = img.resize((width, height), Image.ANTIALIAS)
+image = ImageTk.PhotoImage(imag)
+panel = Label(window, image=image)
+panel.pack(side="top", fill="both", expand="no")
+
+window.mainloop()
+
+
+
+
 window = Tk()
 window.title("Добро пожаловать в приложение PythonRu")
 window.geometry('400x250')
-lbl = Label(window, text="Привет", font=("Arial Bold", 40))
+lbl = Label(window, text="Здарова", font=("Arial Bold", 40))
 lbl.grid(column=0, row=0)
 btn = Button(window, text="Не нажимать!", command=clicked2)
 btn.grid(column=1, row=0)
@@ -30,7 +77,7 @@ btn.grid(column=0, row=1)
 
 combo = Combobox(window)
 combo['values'] = [btn.grid, 2, 3, 4, 5, "Текст"]
-combo.current(1)  # установите вариант по умолчанию
+combo.current(1)
 combo.grid(column=100, row=0)
 window.mainloop()
 
@@ -39,7 +86,7 @@ window = Tk()
 window.title("Добро пожаловать в приложение PythonRu")
 window.geometry('400x250')
 chk_state1 = BooleanVar()
-chk_state1.set(True)  # задайте проверку состояния чекбокса
+chk_state1.set(True)
 chk_state2 = BooleanVar()
 chk_state2.set(True)
 chk_state3 = BooleanVar()
@@ -53,7 +100,7 @@ chk3.grid(column=0, row=2)
 window.mainloop()
 
 window = Tk()
-window.title("Добро пожаловать в приложение PythonRu")
+window.title("Акации по акции")
 window.geometry('400x250')
 rad1 = Radiobutton(window, text='Первый', value=1)
 rad2 = Radiobutton(window, text='Второй', value=2)
@@ -83,3 +130,7 @@ rad3.grid(column=2, row=0)
 btn.grid(column=3, row=0)
 lbl.grid(column=0, row=1)
 window.mainloop()
+
+
+
+
